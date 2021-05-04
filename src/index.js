16f806fe56ff01d9072d4836ad0a5a2ce98c64ba
Sheridan <sheridanmaustad@gmail.com>
2021-05-04 08:48:57 -0600

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const wholeDocument = document.querySelector("#main-content")
+  console.log(wholeDocument)
+  
+  const sheep = document.getElementById("create-task-form");
+  console.log (sheep);
+
+  const myToDoList = document.getElementById("tasks")
+  console.log(myToDoList)
+
+  sheep.addEventListener("submit", function(iHateThis){
+    iHateThis.preventDefault()
+    console.log(iHateThis.target)
+
+    const inputBox = document.getElementById("new-task-description").value;
+  console.log(inputBox)
+
+const listInput = document.createElement("li")
+    listInput.textContent = inputBox
+    
+    myToDoList.appendChild(listInput)
+
+    sheep.reset();
+  })
+
 });
+
